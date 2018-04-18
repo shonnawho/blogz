1069cbb0a73498d7808c0093a509d98398bf0c36
Shonna <peshonna@gmail.com>
2018-04-17 23:43:54 -0500

    link put on title,blog list was able to return

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 import os
 import jinja2
 from flask_sqlalchemy import SQLAlchemy
@@ -7,10 +7,11 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 's1i9r8p6'
+
 
 
 db = SQLAlchemy(app)
-
 
 #class for database
 class Blog(db.Model):
@@ -19,17 +20,16 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
 
-    def __init__(self,title,body):
+    def __init__(self,title,body,blog):
         self.title = title
         self.body = body
+        
 
-
+ @app.route('/')
+def index():
+    blog_page = request.args.get('')
+    return redirect('/blog')
     
-
-
-
-
-
 @app.route('/newpost', methods=['POST','GET'])
 def newpost():
 
@@ -37,44 +37,33 @@ def newpost():
     blog_title = '' 
     
     if request.method == 'POST':
+
         blog_body = request.form['blog_body']
         blog_title = request.form['blog_title']
+        
         return render_template('newpost.html', title='Blogs', blog_body=blog_body,blog_title=blog_title)
-        #blog = [blog_title, blog_body]
-
-    
-        #return redirect("/blog")
+        
     else:
         
         
-        return render_template('newpost.html')
-
-#submit the 
+        return render_template('newpost.html', title="Blogs")
 
 
-
-
-
-
-
-
-#returning the information that is the Newpost form
-
-@app.route('/blog', methods=['POST','GET'])
+@app.route('/blog', methods=['POST', 'GET'])
 def blog():
+    
     if request.method == 'POST':
         
         blog_body = request.form['blog_body']
         blog_title = request.form['blog_title']
-        
-        #returns the blog.html with the title and the body
-        return render_template('blog.html', blog_title=blog_title,blog_body=blog_body)
+        new_blog = Blog(blog_title, blog_body,blog)
+        db.session.add(new_blog)
+        db.session.commit()
+        #request.args.get('blog_list')
 
-    #else:
-
-        #if 
-
-
+    blog_list = Blog.query.filter_by().all()
+    return render_template('blog.html',blog_list=blog_list)
+    
 
 if __name__ == '__main__':
 
